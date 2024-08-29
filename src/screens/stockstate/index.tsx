f0e01f9f39909db9d8a5caef36d1876311f010f7
wonbeanie <goobb2580@gmail.com>
2024-08-29 17:59:10 +0900

    공백 추가

--- src/screens/stockstate/index.tsx
+++ src/screens/stockstate/index.tsx
@@ -5,27 +5,27 @@ import StockDetails from '../../components/screens/stockstate/StockDetails'
 import styled from '@emotion/styled'
 
 export default function StockState() {
-  return (
-    <div css={container}>
-        <div css={{
-            borderBottom : "1px solid #dddddd",
-        }}>
-            <StockInfo />
-            <StockDetails />
+    return (
+        <div css={container}>
+            <div css={{
+                borderBottom : "1px solid #dddddd",
+            }}>
+                <StockInfo />
+                <StockDetails />
+            </div>
+            <div css={{
+                marginTop : 30,
+                borderBottom : "1px solid #dddddd",
+            }}>
+                <StockInfo />
+                {/* <StockDetails /> */}
+            </div>
+            <div css={{
+                marginTop : 30,
+            }}>
+                <StockInfo />
+                {/* <StockDetails /> */}
+            </div>
         </div>
-        <div css={{
-            marginTop : 30,
-            borderBottom : "1px solid #dddddd",
-        }}>
-            <StockInfo />
-            {/* <StockDetails /> */}
-        </div>
-        <div css={{
-            marginTop : 30,
-        }}>
-            <StockInfo />
-            {/* <StockDetails /> */}
-        </div>
-    </div>
-  )
+    )
 }
