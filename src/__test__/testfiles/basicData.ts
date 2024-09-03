fe12cf2f365577dc81e58f16e0cd40a1499e6429
wonbeanie <goobb2580@gmail.com>
2024-09-03 22:51:17 +0900

    테스트 코드 작성시 필요한 데이터 파일 세팅
    데이터 파일 변환 기능 테스트

---
+++ src/__test__/testfiles/basicData.ts
@@ -0,0 +1,317 @@
+export default [
+    {
+        stockName : "Financial Select Sector SPDR ETF US81369Y6059",
+        financial : {
+            totalPrice : "122",
+            buyFirstTime : "2023.07.05 (12:56:55)",
+            installmentSavingRate : "0",
+            depositsRate : "0",
+            totalReturn : "0",
+            totalDividend : "0"
+        },
+        stockState : {
+            totalPrice : "122",
+            profit : "0",
+            flatPrice : "40",
+            retentionTime : "1년 2개월",
+            amount : "3",
+            buyFirstTime : "2023.07.05 (12:56:55)",
+            currentPrice : "0"
+        },
+        stockHistory : {
+            totalPricePercent : "0",
+            totalReturn : "0",
+            currentPrice : "0",
+            history : [
+                {
+                    conclusionAmount : "122",
+                    historyProfit : "-",
+                    conclusionDate : "2023.07.05 (12:56:55)",
+                    unitPrice : "40",
+                    detailContext : "외화증권매수",
+                    tradeType : "매수",
+                    quantity : "3"
+                }
+            ]
+        }
+    },
+    {
+        stockName : "iShares Expanded Tech Sector ETF US4642875490",
+        financial : {
+            totalPrice : "555",
+            buyFirstTime : "2023.02.15 (11:23:52)",
+            installmentSavingRate : "0",
+            depositsRate : "0",
+            totalReturn : "482",
+            totalDividend : "0"
+        },
+        stockState : {
+            totalPrice : "555",
+            profit : "0",
+            flatPrice : "92",
+            retentionTime : "2년 10개월",
+            amount : "3",
+            buyFirstTime : "2023.02.15 (11:23:52)",
+            currentPrice : "0"
+        },
+        stockHistory : {
+            totalPricePercent : "0",
+            totalReturn : "482",
+            currentPrice : "0",
+            history : [
+                {
+                    conclusionAmount : "122",
+                    historyProfit : "482",
+                    conclusionDate : "2023.07.04 (14:45:12)",
+                    unitPrice : "108",
+                    detailContext : "외화증권매도",
+                    tradeType : "매도",
+                    quantity : "3"
+                },
+                {
+                    conclusionAmount : "555",
+                    historyProfit : "-",
+                    conclusionDate : "2023.02.15 (11:23:52)",
+                    unitPrice : "92",
+                    detailContext : "외화증권매수",
+                    tradeType : "매수",
+                    quantity : "6"
+                }
+            ]
+        }
+    },
+    {
+        stockName : "Invesco NASDAQ 100 ETF US46138G6492",
+        financial : {
+            totalPrice : "1886",
+            buyFirstTime : "2023.04.24 (11:34:56)",
+            installmentSavingRate : "0",
+            depositsRate : "0",
+            totalReturn : "0",
+            totalDividend : "0"
+        },
+        stockState : {
+            totalPrice : "1886",
+            profit : "0",
+            flatPrice : "186",
+            retentionTime : "2년 8개월",
+            amount : "10",
+            buyFirstTime : "2023.04.24 (11:34:56)",
+            currentPrice : "0"
+        },
+        stockHistory : {
+            totalPricePercent : "0",
+            totalReturn : "0",
+            currentPrice : "0",
+            history : [
+                {
+                    conclusionAmount : "353",
+                    historyProfit : "-",
+                    conclusionDate : "2023.07.04 (12:46:48)",
+                    unitPrice : "176",
+                    detailContext : "외화증권매수",
+                    tradeType : "매수",
+                    quantity : "2"
+                },
+                {
+                    conclusionAmount : "955",
+                    historyProfit : "-",
+                    conclusionDate : "2023.04.26 (09:23:15)",
+                    unitPrice : "191",
+                    detailContext : "외화증권매수",
+                    tradeType : "매수",
+                    quantity : "5"
+                },
+                {
+                    conclusionAmount : "578",
+                    historyProfit : "-",
+                    conclusionDate : "2023.04.24 (11:34:56)",
+                    unitPrice : "192",
+                    detailContext : "외화증권매수",
+                    tradeType : "매수",
+                    quantity : "3"
+                }
+            ]
+        }
+    },
+    {
+        stockName : "Global X Robotcs Artificial Intellig ETF US37954Y7159",
+        financial : {
+            totalPrice : "269",
+            buyFirstTime : "2023.06.29 (14:12:36)",
+            installmentSavingRate : "0",
+            depositsRate : "0",
+            totalReturn : "0",
+            totalDividend : "0"
+        },
+        stockState : {
+            totalPrice : "269",
+            profit : "0",
+            flatPrice : "26",
+            retentionTime : "2년 6개월",
+            amount : "10",
+            buyFirstTime : "2023.06.29 (14:12:36)",
+            currentPrice : "0"
+        },
+        stockHistory : {
+            totalPricePercent : "0",
+            totalReturn : "0",
+            currentPrice : "0",
+            history : [
+                {
+                    conclusionAmount : "269",
+                    historyProfit : "-",
+                    conclusionDate : "2023.06.29 (14:12:36)",
+                    unitPrice : "26",
+                    detailContext : "외화증권매수",
+                    tradeType : "매수",
+                    quantity : "10"
+                }
+            ]
+        }
+    },
+    {
+        stockName : "동서 026960",
+        financial : {
+            totalPrice : "260,980",
+            buyFirstTime : "2023.04.14 (15:54:42)",
+            installmentSavingRate : "0",
+            depositsRate : "0",
+            totalReturn : "0",
+            totalDividend : "0"
+        },
+        stockState : {
+            totalPrice : "260,980",
+            profit : "0",
+            flatPrice : "18,680",
+            retentionTime : "1년 4개월",
+            amount : "14",
+            buyFirstTime : "2023.04.14 (15:54:42)",
+            currentPrice : "0"
+        },
+        stockHistory : {
+            totalPricePercent : "0",
+            totalReturn : "0",
+            currentPrice : "0",
+            history : [
+                {
+                    conclusionAmount : "185,900",
+                    historyProfit : "-",
+                    conclusionDate : "2023.05.16 (10:23:11)",
+                    unitPrice : "18,590",
+                    detailContext : "코스피매수",
+                    tradeType : "매수",
+                    quantity : "10"
+                },
+                {
+                    conclusionAmount : "75,080",
+                    historyProfit : "-",
+                    conclusionDate : "2023.04.14 (15:54:42)",
+                    unitPrice : "18,770",
+                    detailContext : "코스피매수",
+                    tradeType : "매수",
+                    quantity : "4"
+                }
+            ]
+        }
+    },
+    {
+        stockName : "삼성전자우 005935",
+        financial : {
+            totalPrice : "1,421,600",
+            buyFirstTime : "2023.03.17 (17:25:53)",
+            installmentSavingRate : "0",
+            depositsRate : "0",
+            totalReturn : "0",
+            totalDividend : "0"
+        },
+        stockState : {
+            totalPrice : "1,421,600",
+            profit : "0",
+            flatPrice : "64,250",
+            retentionTime : "1년 5개월",
+            amount : "22",
+            buyFirstTime : "2023.03.17 (17:25:53)",
+            currentPrice : "0"
+        },
+        stockHistory : {
+            totalPricePercent : "0",
+            totalReturn : "0",
+            currentPrice : "0",
+            history : [
+                {
+                    conclusionAmount : "127,600",
+                    historyProfit : "-",
+                    conclusionDate : "2023.05.05 (12:32:41)",
+                    unitPrice : "63,800",
+                    detailContext : "코스피매수",
+                    tradeType : "매수",
+                    quantity : "2"
+                },
+                {
+                    conclusionAmount : "1,294,000",
+                    historyProfit : "-",
+                    conclusionDate : "2023.03.17 (17:25:53)",
+                    unitPrice : "64,700",
+                    detailContext : "코스피매수",
+                    tradeType : "매수",
+                    quantity : "20"
+                }
+            ]
+        }
+    },
+    {
+        stockName : "삼성전자우 005935",
+        financial : {
+            totalPrice : "1,421,600",
+            buyFirstTime : "2023.03.17 (17:25:53)",
+            installmentSavingRate : "0",
+            depositsRate : "0",
+            totalReturn : "5,292",
+            totalDividend : "5,292"
+        },
+        stockState : {
+            totalPrice : "1,421,600",
+            profit : "0",
+            flatPrice : "64,250",
+            retentionTime : "1년 5개월",
+            amount : "22",
+            buyFirstTime : "2023.03.17 (17:25:53)",
+            currentPrice : "0"
+        },
+        stockHistory : {
+            totalPricePercent : "0",
+            totalReturn : "5,292",
+            currentPrice : "0",
+            history : [
+                {
+                    conclusionAmount : "5,292",
+                    historyProfit : "-",
+                    conclusionDate : "2023.07.08 (12:12:22)",
+                    unitPrice : "0",
+                    detailContext : "배당금",
+                    tradeType : "입금",
+                    quantity : "0"
+                },
+                {
+                    conclusionAmount : "127,600",
+                    historyProfit : "-",
+                    conclusionDate : "2023.05.05 (12:32:41)",
+                    unitPrice : "63,800",
+                    detailContext : "코스피매수",
+                    tradeType : "매수",
+                    quantity : "2"
+                },
+                {
+                    conclusionAmount : "1,294,000",
+                    historyProfit : "-",
+                    conclusionDate : "2023.03.17 (17:25:53)",
+                    unitPrice : "64,700",
+                    detailContext : "코스피매수",
+                    tradeType : "매수",
+                    quantity : "20"
+                }
+            ]
+        }
+    },
+]
---
+++ src/__test__/testfiles/basicInfo.ts
@@ -0,0 +1,7 @@
+export default {
+    totalInputAmount : "5,475,759",
+    totalReturnRate : "11.8%",
+    currentReturn : "11.8%",
+    installmentSavingRate : "0",
+    depositsRate : "0"
+}
